b8ea3322dda7dc64c2e30cfa6258a5595c3bf60c
asesmero <asesmero@gmail.com>
2023-07-31 13:46:57 +0200

    3X6

---
+++ keyboards/handwired/asesmero/3x6/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x2B, 0x26, 0xB5, 0x94, 0xEC, 0x94, 0xE0, 0x58}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 1, 1 }
