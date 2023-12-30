bfac660fee48590d7dd9bbe3d6ba30f0dd59eb61
asesmero <asesmero@gmail.com>
2023-12-30 14:18:57 +0100

    correciones

--- keyboards/handwired/asesmero/3x3/keymaps/vial/keymap.c
+++ keyboards/handwired/asesmero/3x3/keymaps/vial/keymap.c
@@ -27,37 +27,37 @@ enum layer_names {
  const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[LAYER_0] = LAYOUT_ortho_3x3(
-		MEH(KC_A), MEH(KC_B), MEH(KC_C), 
-		MEH(KC_D), MEH(KC_E), MEH(KC_F), 
+		MEH(KC_A), MEH(KC_B), MEH(KC_C),
+		MEH(KC_D), MEH(KC_E), MEH(KC_F),
 		MEH(KC_G), MEH(KC_H), TO(1)
 	),
 
-	[LAYER_1] = LAYOUT_ortho_6x6(
+	[LAYER_1] = LAYOUT_ortho_3x3(
 		KC_TRNS, KC_TRNS, KC_TRNS,
 		KC_TRNS, KC_TRNS, KC_TRNS,
 		KC_TRNS, KC_TRNS, TO(2)
 	),
 
-	[LAYER_2] = LAYOUT_ortho_6x6(
+	[LAYER_2] = LAYOUT_ortho_3x3(
 		KC_TRNS, KC_TRNS, KC_TRNS,
 		KC_TRNS, KC_TRNS, KC_TRNS,
 		KC_TRNS, KC_TRNS, TO(3)
-	),	
+	),
 
-	[LAYER_3] = LAYOUT_ortho_6x6(
+	[LAYER_3] = LAYOUT_ortho_3x3(
 		KC_TRNS, KC_TRNS, KC_TRNS,
 		KC_TRNS, KC_TRNS, KC_TRNS,
 		KC_TRNS, KC_TRNS, TO(0)
-	)		
+	)
 };
 
 void keyboard_post_init_user(void) {
     rgblight_enable_noeeprom();
     rgblight_sethsv_noeeprom(HSV_RED);
 };
-	
+
 layer_state_t layer_state_set_user(layer_state_t state) {
-    
+
 	switch (get_highest_layer(state)) {
     case LAYER_0:
         rgblight_sethsv_noeeprom(HSV_RED);
